6c9408b91d7d28b38085c7aba4487fbb02049a4b
Erick Alves <erickalvessud@gmail.com>
2020-11-09 14:16:20 -0300

    Configuração do Flyway para criação das tabelas de Carro e acessórios

--- src/main/java/com/deveficiente/jpa/repository/AutorRepository.java
+++ src/main/java/com/deveficiente/jpa/repository/AutorRepository.java
@@ -1,9 +1,11 @@
 package com.deveficiente.jpa.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.deveficiente.jpa.entity.AutorEntity;
 
+@Repository
 public interface AutorRepository extends JpaRepository<AutorEntity, Long> {
 
 }
